64a763987520f9abbf27c4529be53887e6b4eebc
robertmotr <61203332+robertmotr@users.noreply.github.com>
2020-12-29 00:31:33 -0500

    Update Problem4.cpp

--- Problem4.cpp
+++ Problem4.cpp
@@ -19,22 +19,25 @@ int main() {
             vin.push_back(line);
         }
     }
-
     vector<string> passport;
     for(int i = 0; i < vin.size(); i++) {
         if(vin[i] != "\n") {
             passport.push_back(vin[i]);
         }
         else {
+            // todo work with passport vector
+            string total;
+            for(int x = 0; x < passport.size(); x++) {
+                vector<string> tempsplit = splitString(passport[x], ' ');
+                for(int k = 0; k < tempsplit.size(); k++) {
+                    total += tempsplit[k];
+                }
+            }
 
+            // determine if strtotal is valid
             passport.clear();
-
         }
-
-
     }
-
-
     cout << valid_passports << endl;
     return 0;
 }
