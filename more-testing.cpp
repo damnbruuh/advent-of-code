812a4b02ac47601bbb83a294e30a6eb8cf957f44
robertmotr <61203332+robertmotr@users.noreply.github.com>
2020-12-29 13:52:39 -0500

    did some testing

---
+++ more-testing.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string str = "This is a string.";
+
+    int found = str.find("L");
+
+    cout << found << endl;
+
+}
