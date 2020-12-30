9c378aa41ea4a22858dc4f4e0421a27895b25272
robertmotr <61203332+robertmotr@users.noreply.github.com>
2020-12-30 18:26:27 -0500

    finished problem 5 using binary way

--- Problem5.cpp
+++ Problem5.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,6 +9,7 @@ int main() {
 
     string line;
     vector<string> vin;
+    vector<int> ids;
     while(getline(cin, line)) {
         if(line == "stop") {
             break;
@@ -16,6 +18,43 @@ int main() {
             vin.push_back(line);
         }
     }
-    
+
+    for(int i = 0; i < vin.size(); i++) {
+        string str = vin[i];
+        string row = str.substr(0, 7);
+        string column = str.substr(7, 3);
+
+        for(int x = 0; x < row.size(); x++) {
+
+            if(row[x] == 'F') {
+                row[x] = '0';
+            }
+            else {
+                row[x] = '1';
+            }
+        }
+
+        for(int k = 0; k < column.size(); k++) {
+            if(column[k] == 'L') {
+                column[k] = '0';
+            }
+            else {
+                column[k] = '1';
+            }
+        }
+
+        int rown = stoi(row, 0, 2);
+        int coln = stoi(column, 0, 2);
+
+        int seat = rown * 8 + coln;
+
+        ids.push_back(seat);
+    }
+
+    sort(ids.begin(), ids.end(), greater<int>());
+
+    cout << ids[0] << endl;
     return 0;
+
+
 }
