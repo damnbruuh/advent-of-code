7e5dd2648997785cb278aae700365ca5ca8b0d19
robertmotr <61203332+robertmotr@users.noreply.github.com>
2021-05-01 01:56:45 -0400

    finished day 2

--- 2019/day2.cpp
+++ 2019/day2.cpp
@@ -5,24 +5,42 @@
 
 using namespace std;
 
-vector<string> splitString(string s, char delim) {
-    vector<string> tokens;
+vector<int> splitString(string s, char delim)
+{
+    vector<int> tokens;
     string buf;
     stringstream ss(s);
-    while(getline(ss, buf, delim)) {
-        tokens.push_back(buf);
+    while (getline(ss, buf, delim))
+    {
+        tokens.push_back(stoi(buf));
     }
     return tokens;
 }
 
-int main() {
+int main()
+{
+    string a;
+    cin >> a;
+    vector<int> codes = splitString(a, ',');
 
-    string a, b;
-    getline(cin, a);
-    getline(cin, b);
+    codes[1] = 12;
+    codes[2] = 2;
+    
+    for(int i = 0; i < codes.size();) {
 
-    vector<int> opcodes;
-    vector<int> positions;
+        if(codes[i] == 1) {
+            codes[codes[i + 3]] = codes[codes[i + 1]] + codes[codes[i + 2]];
+        }
+        else if(codes[i] == 2) {
+            codes[codes[i + 3]] = codes[codes[i + 1]] * codes[codes[i + 2]];
+        }
+        else if(codes[i] == 99) {
+            break;
+        }
+        i += 4;
+    }
+
+    cout << codes[0] << endl;
 
     return 0;
 }
